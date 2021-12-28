a501f6f1b516c908f8d034f095fab78310b795cc
Tiago Oliveira <tiagolivesan@outlook.com>
2021-12-28 20:30:31 -0300

    Estrutura JSX

--- myskills/App.js
+++ myskills/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Home } from './src/pages/Home';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  )
+  return <Home />
 }
