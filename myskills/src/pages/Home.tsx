7bd69c56aecf37742afad2307d8a67f3e3cc21ba
Tiago Oliveira <tiagolivesan@outlook.com>
2022-01-23 15:23:43 -0300

    Tipando componenetes com Typescript

--- myskills/src/pages/Home.tsx
+++ myskills/src/pages/Home.tsx
@@ -65,7 +65,10 @@ export function Home() {
         onChangeText={setNewSkill}
       />
 
-      <Button onPress={handleAddNewSkill} />
+      <Button
+        onPress={handleAddNewSkill}
+        title="Adicionar"
+      />
 
       <Text style={[styles.title, { marginVertical: 50 }]}>
         My Skills
